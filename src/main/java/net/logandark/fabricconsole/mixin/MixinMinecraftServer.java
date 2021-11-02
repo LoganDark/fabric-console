08a0f4e79ea0a75cc8e2e10bb2c524a4f3ca9124
LoganDark <logandarklock@gmail.com>
2021-11-01 22:09:48 -0700

    Make it run on client

--- src/main/java/net/logandark/fabricconsole/mixin/MixinMinecraftServer.java
+++ src/main/java/net/logandark/fabricconsole/mixin/MixinMinecraftServer.java
@@ -1,16 +1,16 @@
 package net.logandark.fabricconsole.mixin;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.loader.api.FabricLoader;
 import net.logandark.fabricconsole.TextToAnsi;
+import net.minecraft.command.argument.TextArgumentType;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.UUID;
@@ -18,22 +18,32 @@ import java.util.UUID;
 @Mixin(value = MinecraftServer.class)
 public class MixinMinecraftServer {
 	@Shadow
-	@Final
-	private static Logger LOGGER;
+	public CommandManager getCommandManager() {
+		return null;
+	}
 
-	@Inject(
+	@Redirect(
 		method = "sendSystemMessage",
 		at = @At(
 			value = "INVOKE",
 			target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V",
 			remap = false
-		),
-		cancellable = true
+		)
 	)
-	private void fabric_console_onSystemMessage(Text message, UUID senderUuid, CallbackInfo ci) {
-		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
-			LOGGER.info(TextToAnsi.INSTANCE.textToAnsi(message));
-			ci.cancel();
-		}
+	private void fabric_console_onSystemMessage(Logger instance, String s, Text message, UUID senderUuid) {
+		instance.info(TextToAnsi.INSTANCE.textToAnsi(message));
+	}
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void fabric_console_onInit(CallbackInfo ci) {
+		getCommandManager().getDispatcher().register(CommandManager.literal("tellrawc")
+			.requires(src -> src.hasPermissionLevel(2))
+			.then(CommandManager.argument("message", TextArgumentType.text())
+				.executes(ctx -> {
+					ctx.getSource().getMinecraftServer().sendSystemMessage(TextArgumentType.getTextArgument(ctx, "message"), UUID.randomUUID());
+					return 1;
+				})
+			)
+		);
 	}
 }
