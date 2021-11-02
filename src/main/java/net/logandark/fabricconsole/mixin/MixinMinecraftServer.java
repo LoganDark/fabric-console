978ca938fd46f487fdef2c4819ed72a82ff07bdb
LoganDark <logandarklock@gmail.com>
2021-11-01 22:09:48 -0700

    backport changes from 1.16

---
+++ src/main/java/net/logandark/fabricconsole/mixin/MixinMinecraftServer.java
@@ -0,0 +1,47 @@
+package net.logandark.fabricconsole.mixin;
+
+import net.logandark.fabricconsole.TextToAnsi;
+import net.minecraft.command.arguments.TextArgumentType;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = MinecraftServer.class)
+public class MixinMinecraftServer {
+	@Shadow
+	public CommandManager getCommandManager() {
+		return null;
+	}
+
+	@Redirect(
+		method = "sendMessage",
+		at = @At(
+			value = "INVOKE",
+			target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V",
+			remap = false
+		)
+	)
+	private void fabric_console_onSystemMessage(Logger instance, String s, Text message) {
+		instance.info(TextToAnsi.INSTANCE.textToAnsi(message));
+	}
+
+	@Inject(method = "<init>", at = @At("RETURN"))
+	private void fabric_console_onInit(CallbackInfo ci) {
+		getCommandManager().getDispatcher().register(CommandManager.literal("tellrawc")
+			.requires(src -> src.hasPermissionLevel(2))
+			.then(CommandManager.argument("message", TextArgumentType.text())
+				.executes(ctx -> {
+					ctx.getSource().getMinecraftServer().sendMessage(TextArgumentType.getTextArgument(ctx, "message"));
+					return 1;
+				})
+			)
+		);
+	}
+}
